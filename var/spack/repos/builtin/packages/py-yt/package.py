167527e7bf0444e623423a970bcfc34be9bde84a
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2017-01-03 16:07:12 -0600

    Add rockstar support to py-yt package

--- var/spack/repos/builtin/packages/py-yt/package.py
+++ var/spack/repos/builtin/packages/py-yt/package.py
@@ -54,6 +54,7 @@ class PyYt(PythonPackage):
     variant("astropy", default=True, description="enable astropy support")
     variant("h5py", default=True, description="enable h5py support")
     variant("scipy", default=True, description="enable scipy support")
+    variant("rockstar", default=False, description="enable rockstar support")
 
     depends_on("py-astropy", type=('build', 'run'), when="+astropy")
     depends_on("py-cython", type=('build', 'run'))
@@ -64,7 +65,17 @@ class PyYt(PythonPackage):
     depends_on("py-scipy", type=('build', 'run'), when="+scipy")
     depends_on("py-setuptools", type="build")
     depends_on("py-sympy", type=('build', 'run'))
+    depends_on("rockstar@yt", type=('build', 'run'), when="+rockstar")
     depends_on("python @2.7:2.999,3.4:")
+
+    def install(self, spec, prefix):
+        if '+rockstar' in spec:
+            if os.path.exists('rockstar.cfg'):
+                rm('rockstar.cfg')
+            rockstar_cfg = open('rockstar.cfg', 'w')
+            rockstar_cfg.write(os.environ['ROCKSTAR_DIR']+"/lib\n")
+        setup_py("install", "--prefix=%s" % prefix)
+        self.check_install(spec, prefix)
 
     @run_after('install')
     def check_install(self):
